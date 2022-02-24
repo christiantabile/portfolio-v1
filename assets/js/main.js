95ddaa9f69acdaf50386ed158ded4a3ba0168fc2
catmac <christiantabile@gmail.com>
2022-02-23 21:51:27 -0800

    Work on overall project. Add bundlers for swiper script. Add landing page. Add various asset files.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,29 +3,118 @@ const navMenu = document.getElementById('nav-menu'),
     navToggle = document.getElementById('nav-toggle'),
     navClose = document.getElementById('nav-close')
 
-    /*========== MENU SHOW ==========*/
-    /* Validate if constant exist */
-    if(navToggle){
-        navToggle.addEventListener('click', () => {
-            navMenu.classList.add('show-menu')
-        })
-    }
+/*========== MENU SHOW ==========*/
+/* Validate if constant exist */
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
 
-    /*==== MENU HIDDEN ====*/
-    /* Validate if constant exists */
-    if(navClose){
-        navClose.addEventListener('click', () => {
-            navMenu.classList.remove('show-menu')
-        })
-    }
-
-    /*=========== REMOVE MOBILE MENU =========*/
-    const navLink = document.querySelectorAll('.nav__link')
-
-    function linkAction(){
-        const navMenu = document.getElementById('nav-menu')
-        // When we click on each nav__link, we remove the show-menu class
+/*==== MENU HIDDEN ====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
-    }
+    })
+}
 
-    navLink.forEach(n => n.addEventListener('click', linkAction))
+/*=========== REMOVE MOBILE MENU =========*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=========== PROJECT SWIPER =========*/
+let swiper = new Swiper('.project__container', {
+    cssMode: true,
+    loop: true,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
+/*=========== SCROLL SECTIONS ACTIVE LINK =========*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=========== CHANGE BACKGROUND HEADER =========*/
+function scrollHeader(){
+    const nav = document.getElementById('header');
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+
+window.addEventListener('scroll', scrollHeader)
+
+/*=========== SHOW SCROLL UP =========*/
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll to the <a> tag wiht the scroll-top class
+    if (this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+
+window.addEventListener('scroll', scrollUp);
+
+/*=========== DARK/LIGHT THEME =========*/
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+// We validated if the user previously chose a topic
+if (selectedTheme) {
+    // If the validation is fulfilled, we ask twhat the issue was to know if we activated or deactived the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon == 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark/icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+let clock = document.getElementById("live-clock");
+
+setInterval(() => {
+  let date = new Date();
+  clock.innerHTML = date.toLocaleTimeString();
+}, 1000);
